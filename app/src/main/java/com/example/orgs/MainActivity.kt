b21b5570815debfde436edad2cccb1e96b163d44
Pedro Alvim <pedro@sentry.com.br>
2022-08-18 17:01:06 -0300

    Finalizada aula Implementando o layout do android

--- app/src/main/java/com/example/orgs/MainActivity.kt
+++ app/src/main/java/com/example/orgs/MainActivity.kt
@@ -1,4 +1,13 @@
 package com.example.orgs
 
-class MainActivity {
+import android.app.Activity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
 }
