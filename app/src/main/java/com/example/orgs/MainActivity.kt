33b9caef79c63a18e18ad8c25e2b6d66f2e16d09
Pedro Alvim <pedro@sentry.com.br>
2022-08-19 17:59:58 -0300

    Terminada a aula buscando as views do layout

--- app/src/main/java/com/example/orgs/MainActivity.kt
+++ app/src/main/java/com/example/orgs/MainActivity.kt
@@ -9,5 +9,11 @@ class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val nome = findViewById<TextView>(R.id.nome)
+        nome.text = "Cesta de frutas"
+        val descricao = findViewById<TextView>(R.id.descricao)
+        descricao.text = "Laranja, manga e maça"
+        val valor = findViewById<TextView>(R.id.valor)
+        valor.text = "19,99"
     }
 }
