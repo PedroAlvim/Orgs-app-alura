2e53627833e8c797e1dcafb7a493867f3fff08d2
Pedro Alvim <pedro@sentry.com.br>
2022-08-22 14:30:08 -0300

    Finalizada a aula: Conhecendo o ciclo de vida da Activity

---
+++ app/src/main/java/com/example/orgs/dao/ProdutosDao.kt
@@ -0,0 +1,20 @@
+package com.example.orgs.dao
+
+
+import com.example.orgs.model.Produtos
+
+class ProdutosDao {
+
+    fun adiciona(produto: Produtos){
+        produtos.add(produto)
+    }
+
+
+    fun buscaTodos(): List<Produtos> {
+        return produtos.toList()
+    }
+
+    companion object {
+        private val produtos = mutableListOf<Produtos>()
+    }
+}
--- app/src/main/java/com/example/orgs/ui/activity/FormularioProdutoActivity.kt
+++ app/src/main/java/com/example/orgs/ui/activity/FormularioProdutoActivity.kt
@@ -2,8 +2,49 @@ package com.example.orgs.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
+import android.util.LogPrinter
+import android.widget.Button
+import android.widget.EditText
 import com.example.orgs.R
+import com.example.orgs.dao.ProdutosDao
+import com.example.orgs.model.Produtos
+import java.math.BigDecimal
 
 class FormularioProdutoActivity : AppCompatActivity(R.layout.activity_formulario_produto) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val botaoSalvar = findViewById<Button>(R.id.botao_salvar)
+        botaoSalvar.setOnClickListener {
+            val campoNome = findViewById<EditText>(R.id.nome)
+            val nome = campoNome.text.toString()
+            val campoDescricao = findViewById<EditText>(R.id.descricao)
+            val descricao = campoDescricao.text.toString()
+            val campoValor = findViewById<EditText>(R.id.valor)
+            val valorEmTexto = campoValor.text.toString()
+
+            val valor = if(valorEmTexto.isBlank()){
+                BigDecimal.ZERO
+            }else{
+                BigDecimal(valorEmTexto)
+            }
+
+            val produtoNovo = Produtos(
+                nome = nome,
+                descricao = descricao,
+                valor = valor
+            )
+
+            Log.i("FormularioProduto", "onCreate: $produtoNovo")
+            val dao = ProdutosDao()
+            dao.adiciona(produtoNovo)
+            Log.i("FormularioProduto", "onCreate: ${dao.buscaTodos()}")
+            finish()
+        }
+
+    }
 
 }
